13595c7c541c131deb509c1f4c0cfa6980c4477d
admin <admin@TA-8215-01.kursdom.net>
2019-08-12 15:34:13 +0200

    Add project files.

---
+++ 40SimpleVariabler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _40SimpleVariabler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
