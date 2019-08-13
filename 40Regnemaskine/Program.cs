870afed1fb46d65696dbee36de37380e4cdc3e06
Allan <admin@TA-8215-01>
2019-08-13 09:56:36 +0200

    Opstart af regnemaskine

---
+++ 40Regnemaskine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _40Regnemaskine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Indtast tal 1");
+            string tal1 = Console.ReadLine();
+
+            Console.WriteLine("Indtast tal 2");
+            string tal2 = Console.ReadLine();
+
+            double t1 = Convert.ToDouble(tal1);
+            double t2 = Convert.ToDouble(tal2);
+
+            double res1 = t1 + t2;
+                  
+            Console.WriteLine($"Resultatet er {res1.ToString("N2")}");
+
+
+
+
+        }
+    }
+}
--- 40SimpleVariabler/Program.cs
+++ 40SimpleVariabler/Program.cs
@@ -5,12 +5,41 @@ namespace _40SimpleVariabler
     class Program
     {
         static void Main(string[] args)
-        {
-            int a = 10;
+        {                    
 
-            Console.WriteLine();
+            {
+                int a = 10;
+                a++;       
+                Console.WriteLine($"a = {a}");
+            }
+            {
+                int a = 10;
+                a--;
+                Console.WriteLine(a);
+            }
+            {
+                int a = 10;
+                a += 20;
+                Console.WriteLine(a);
+            }
+            {
+                double a = 12.5;
+                a++;
+                Console.WriteLine(a);
 
+            }
+            {
+                double a = 12.5;
+                a--;
+                Console.WriteLine(a);
 
+            }
+            {
+                double a = 12.5;
+                a*=2;
+                Console.WriteLine(a);
+
+            }
         }
     }
 }
