233e4a73e382d9dc9f8ff87d5056c2ea3ad2df51
Allan <admin@TA-8215-01>
2019-08-13 14:02:41 +0200

    BreakContinue tilføjet

---
+++ 50BreakContinue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _50BreakContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 19; i = i+3) //Min egen løsning
+            {
+                if (i == 18) break;
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 3 != 0) //modulus tager brøk resten og tjekker her om den er forskellig fra nul, hvis ja så start forfra, ellers print
+                    continue;
+
+                if (i > 15)
+                    break;
+
+                Console.WriteLine(i);
+            }
+        }   
+    }
+}
