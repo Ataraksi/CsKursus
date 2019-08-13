8eb95d83f95b164353f8a4cdb89c3eba275957a2
Allan <admin@TA-8215-01>
2019-08-13 15:59:11 +0200

    Slut dag 2

---
+++ 65EgneExceptions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _65EgneExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
