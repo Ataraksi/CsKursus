ac4a16351d21d376322011ad244a27e33acda87c
Allan Løwe <ataraksi@gmail.com>
2019-08-26 13:24:44 +0200

    75_1 færdig

---
+++ 70_2ArraySplit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _70_2ArraySplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "3;5;22;1;10;15;5";
+            //txt.Split.ToString.
+        }
+    }
+}
---
+++ 75_1SimpleSamlinger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _75_1SimpleSamlinger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lst1 = new List<int>();
+            lst1.Add(3);
+            lst1.Add(1);
+            lst1.Add(8);
+
+            foreach (var item in lst1)
+            {
+                Console.WriteLine(item);
+            }
+
+            Dictionary<int, string> lst2 = new Dictionary<int, string>();
+            lst2.Add(1, "M");
+            lst2.Add(2, "T");
+            lst2.Add(3, "P");
+
+            var p = lst2[2]; // Det er IKKE pladsen i arrayet, men derimod min egen nøgle
+            Console.WriteLine(p);
+
+        }
+    }
+}
