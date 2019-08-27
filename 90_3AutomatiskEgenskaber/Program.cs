4b85657e455f18a813df44460aed4554959d2ac1
Allan Løwe <ataraksi@gmail.com>
2019-08-27 14:32:29 +0200

    90_3 færdig

---
+++ 90_3AutomatiskEgenskaber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _90_3AutomatiskEgenskaber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Fornavn = "Allan";
+            p.Efternavn = "Johansen";
+            Console.WriteLine(p.FuldtNavn());
+        }
+    }
+    class Person
+    {
+        public string Fornavn { get; set; }
+
+        private string efternavn;
+
+        public string Efternavn
+        {
+            get { return efternavn; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    efternavn = "";
+                }
+                else
+                {
+                    efternavn = value;
+                }
+            }
+        }
+
+        public string FuldtNavn()
+        {
+            return Fornavn + " " + Efternavn;
+        }
+
+    }
+}
---
+++ 90_yEgenskaber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _90_yEgenskaber
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+        }
+
+        //class Person
+
+        //private int alder;
+
+        //public int
+
+
+        //propfull
+        //private string _navn;
+
+        //public string MyProperty
+        //    {
+        //        get { return _n_navn; }
+        //        set { _n_navn = value; }
+        //    }
+
+
+
+    }
+}
