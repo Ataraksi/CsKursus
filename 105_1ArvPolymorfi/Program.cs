742d147abfd7579f429c76cbd4d62ce46ce1573a
Allan Løwe <ataraksi@gmail.com>
2019-09-09 13:05:16 +0200

    110_1 færdig

---
+++ 105_1ArvPolymorfi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _105_1ArvPolymorfi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Faktura
+    {
+
+    }
+}
