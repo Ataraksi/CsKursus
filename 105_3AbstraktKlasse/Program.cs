550bbb81d95b311623435ecce18a3c720669598b
Allan Løwe <ataraksi@gmail.com>
2019-09-09 10:15:09 +0200

    105_3 færdig

---
+++ 105_3AbstraktKlasse/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+
+namespace _105_3AbstraktKlasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kursist k = new Kursist() { Id = 1, Navn = "Allan" };
+            k.Skriv1();
+            k.Skriv2();
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+    abstract class Person
+    {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+
+        public void Skriv1()
+        {
+            Console.WriteLine($"Person med id {Id} og navn {Navn}");
+        }
+
+        public abstract void Skriv2();
+    }
+
+    class Kursist : Person
+    {
+        public override void Skriv2()
+        {
+            Console.WriteLine($"Kursist med id {Id} og navn {Navn}");
+        }
+    }
+}
